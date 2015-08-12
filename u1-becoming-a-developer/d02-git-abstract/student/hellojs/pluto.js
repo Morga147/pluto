39061a90d242e04d651b65e8692c98ac96d3e4aa
Joy Morgan <Tesoro1021@gmail.com>
2015-08-12 09:08:32 -0400

    hw2

---
+++ u1-becoming-a-developer/d02-git-abstract/student/hellojs/pluto.js
@@ -0,0 +1,12 @@
+// 1st Prompt
+var name = prompt("What is your name?");
+
+// 1st Alert
+alert(("take out the trash, " + name).toUpperCase() + "!!!");
+
+// 2nd Prompt
+var bags = parseInt(prompt("How many bags of trash are there?"));
+var minutes = bags * 5;
+
+// 2nd Alert 
+ alert("It should take you " + minutes + " minutes.");
