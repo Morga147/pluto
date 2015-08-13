dc1c0b259e44666342522cb4f4f70ab5470e6b9c
Joy Morgan <Tesoro1021@gmail.com>
2015-08-13 17:16:23 -0400

    Everything I got done by happy hour

---
+++ u1-becoming-a-developer/d04-funky-loops/homework/mta.js
@@ -0,0 +1,15 @@
+// Part 4
+
+// Given arrays
+var nLineStops   = ['Times Square', '34th', '28th-n', '23rd-n', 'Union Square', '8th'];
+var lLineStops   = ['8th Ave', '6th Ave', 'Union Square', '3rd Ave', '1st Ave'];
+var sixLineStops = ['Grand Central', '33rd', '28th-six', '23rd-six', 'Union Square', 'Astor Place'];
+
+// Personally created variables
+var getOn, getOff, numOfStops;
+getOn = prompt("The N Line stops at: Times Square, 34th, 28th, 23rd, Union Square, and 8th. \n Where are you boarding?");
+getOff = prompt("Where are you getting off?");
+
+numOfStops = nLineStops.indexOf(getOff) - nLineStops.indexOf(getOn);
+
+window.alert("You will be travelling " + numOfStops + ".");
