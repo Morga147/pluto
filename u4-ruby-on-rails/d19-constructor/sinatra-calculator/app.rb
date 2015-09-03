5202a3d7c591c3ec0c4ac1f12c2f32fe65a43b9c
Joy Morgan <Tesoro1021@gmail.com>
2015-09-03 18:04:17 -0400

    Sinatra Homework

--- u4-ruby-on-rails/d19-constructor/sinatra-calculator/app.rb
+++ u4-ruby-on-rails/d19-constructor/sinatra-calculator/app.rb
@@ -19,5 +19,38 @@ module SinatraCalculator
       "<h1>Hi there, welcome to the Sinatra Calculator, for all your calculating needs.<h1>"
     end
 
+    # get '/calc/add/:num1/:num2' do
+    #   calculator = Calculator.new
+    #   "#{calculator.add(params[:num1],params[:num2])}"
+    # end # end addition
+    #
+    # get '/calc/subtract/:num1/:num2' do
+    #   calculator = Calculator.new
+    #   "#{calculator.subtract(params[:num1],params[:num2])}"
+    # end # end subtraction
+    #
+    # get '/calc/multiply/:num1/:num2' do
+    #   calculator = Calculator.new
+    #   "#{calculator.multiply(params[:num1],params[:num2])}"
+    # end #end multiplication
+    #
+    # get '/calc/divide/:num1/:num2' do
+    #   calculator = Calculator.new
+    #   "#{calculator.divide(params[:num1],params[:num2])}"
+    # end #end division
+
+    #Refactoring for num. 4
+    get '/calc/:operator/:num1/:num2' do |operator, num1, num2|
+      calculator = Calculator.new
+      if (operator == +)
+        "#{calculator.add(params[num1], params[num2])}"
+      elsif (operator == -)
+        "#{calculator.subtract(params [num1], params[num2])}"
+      elsif (operator == *)
+        "#{calculator.multiply(params[num1], params[num2])}"
+      elsif (operator == /)
+        "#{calculator.divide(params[num1], params[num2])}"
+    end # end if/else statements
+  end #end refactoring get...do...
   end #end class
 end #end module
--- u4-ruby-on-rails/d19-constructor/sinatra-calculator/calculator.rb
+++ u4-ruby-on-rails/d19-constructor/sinatra-calculator/calculator.rb
@@ -1,8 +1,21 @@
 class Calculator
 
-	#Giving this to you because I'm really nice...
-	# def add(x, y)
-	#   x.to_f + y.to_f
-	# end
+	# Giving this to you because I'm really nice...
+	def add(x, y)
+	  "Sum: #{x.to_f + y.to_f}"
+	end #end add
+
+	def subtract(x, y)
+		"Difference: #{x.to_f - y.to_f}"
+	end #end subtract
+
+	def multiply(x, y)
+		"Product: #{x.to_f * y.to_f}"
+	end #end multiply
+
+	def divide(x, y)
+	"Quotient: #{x.to_f / y.to_f}"
+	end
+
 
 end
