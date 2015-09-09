b45acfd371130f8182c55bffcf9d466a4fc03632
Joy Morgan <Tesoro1021@gmail.com>
2015-09-09 19:17:20 -0400

    September 9th SQL Rep Homework

--- u4-ruby-on-rails/d23-sea-quail/student/ruby_powered_shopping_spree_hw/ruby_powered_shopping_spree.rb
+++ u4-ruby-on-rails/d23-sea-quail/student/ruby_powered_shopping_spree_hw/ruby_powered_shopping_spree.rb
@@ -1,5 +1,7 @@
 require 'pry'
 require 'pg'
+
+$pg_conn = PG.connect(dbname: 'shopping_db')
 #======================
 # Starter Data
 #======================
@@ -25,4 +27,14 @@ items = [
   "Teach Like a Champion",
   "Practice Perfect"
 ]
+5.times do
+  insert_statement = "INSERT INTO receipts(store, item, number_of_items, price, buy_date) VALUES ($1,$2,$3,$4,$5) RETURNING *"
+  $pg_conn.exec_params(insert_statement,[stores.sample,items.sample,quantities.sample,prices.sample,Date.today])
+end
 
+$pg_conn.exec_params("UPDATE receipts SET item='Giants Jersey' WHERE item='49ers Jersey'")
+$pg_conn.exec_params("UPDATE receipts SET item='PS4', price='$350' WHERE price='$400'")
+
+partfour = $pg_conn.exec_params("UPDATE receipts SET returned=true WHERE id IN (SELECT id FROM receipts ORDER BY random() LIMIT 8)")
+
+$pg_conn.finish
