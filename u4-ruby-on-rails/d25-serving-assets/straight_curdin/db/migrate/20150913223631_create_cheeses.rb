7764455abc7d43e92f959051f004db0c5ddb6410
Joy Morgan <Tesoro1021@gmail.com>
2015-09-14 08:49:47 -0400

    Straight Curdin' Homework

---
+++ u4-ruby-on-rails/d25-serving-assets/straight_curdin/db/migrate/20150913223631_create_cheeses.rb
@@ -0,0 +1,12 @@
+class CreateCheeses < ActiveRecord::Migration
+  def change
+    create_table :cheeses do |t|
+      t.string :name
+      t.string :milk_type
+      t.string :description
+      t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
+  end
+end
